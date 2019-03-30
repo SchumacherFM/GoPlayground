ecf00448f82944d6cfd4546aa3241d7db2094c81
Cyrill Schumacher <cyrill@schumacher.fm>
2019-03-30 08:17:12 +0100

    Update large file bufio Reader tests
    Buffered 16k wins in speed on a new MacPook pro
    
    Implemented them wrong. Do'h!

--- largefile_test.go
+++ largefile_test.go
@@ -11,47 +11,62 @@ import (
 	"gopkg.in/bufio.v1"
 )
 
+// to generate this file:
+// $ dd if=/dev/urandom of=test.txt bs=1048576 count=100
+// for 100MB
 const fileName = "test.txt"
 
 func BenchmarkFileReading(b *testing.B) {
 
 	b.Run("unbuffered", func(b *testing.B) {
-		f, err := os.Open(fileName)
-		if err != nil {
-			b.Fatal(err)
-		}
-		defer f.Close()
 		for i := 0; i < b.N; i++ {
+			f, err := os.Open(fileName)
+			if err != nil {
+				b.Fatal(err)
+			}
 			if _, err := io.Copy(ioutil.Discard, f); err != nil {
 				b.Fatal(err)
 			}
-		}
-	})
-	b.Run("buffered 64k", func(b *testing.B) {
-		f, err := os.Open(fileName)
-		if err != nil {
-			b.Fatal(err)
-		}
-		defer f.Close()
-		fb := bufio.NewReaderSize(f, 1024*64)
-		for i := 0; i < b.N; i++ {
-			if _, err := io.Copy(ioutil.Discard, fb); err != nil {
-				b.Fatal(err)
-			}
+			f.Close()
 		}
 	})
 	b.Run("buffered 16k", func(b *testing.B) {
-		f, err := os.Open(fileName)
-		if err != nil {
-			b.Fatal(err)
-		}
-		defer f.Close()
-		fb := bufio.NewReaderSize(f, 1024*16)
 		for i := 0; i < b.N; i++ {
+			f, err := os.Open(fileName)
+			if err != nil {
+				b.Fatal(err)
+			}
+			fb := bufio.NewReaderSize(f, 1024*16)
 			if _, err := io.Copy(ioutil.Discard, fb); err != nil {
 				b.Fatal(err)
 			}
+			f.Close()
 		}
 	})
-
+	b.Run("buffered 64k", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			f, err := os.Open(fileName)
+			if err != nil {
+				b.Fatal(err)
+			}
+			fb := bufio.NewReaderSize(f, 1024*64)
+			if _, err := io.Copy(ioutil.Discard, fb); err != nil {
+				b.Fatal(err)
+			}
+			f.Close()
+		}
+	})
+	b.Run("buffered 128k", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			f, err := os.Open(fileName)
+			if err != nil {
+				b.Fatal(err)
+			}
+			fb := bufio.NewReaderSize(f, 1024*128)
+			if _, err := io.Copy(ioutil.Discard, fb); err != nil {
+				b.Fatal(err)
+			}
+			f.Close()
+		}
+	})
 }
