24b0ab3b79f9d66b01c019af7c203f8f629e04da
Cyrill Schumacher <cyrill@schumacher.fm>
2020-05-03 21:55:32 +0200

    ct daily downloads

---
+++ comptrain/dailyDownload.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+
+	"golang.org/x/sync/errgroup"
+)
+
+func main() {
+	// https://comptrain.co/home_gym/home-gym-saturday-5-2-2020/
+	// https://comptrain.co/daily_programs/
+
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Minute)
+	defer cancel()
+
+	g, ctx := errgroup.WithContext(ctx)
+
+	g.Go(func() error {
+		err := mainDownload(ctx, "https://comptrain.co/daily_programs/", "wod")
+		if err != nil {
+			println(err.Error())
+		}
+		return nil
+	})
+	g.Go(func() error {
+		err := mainDownload(ctx, "https://comptrain.co/home_gym/home-gym-", "hg")
+		if err != nil {
+			println(err.Error())
+		}
+		return nil
+	})
+	g.Wait()
+}
+
+func mainDownload(ctx context.Context, urlPrefix, filePrefix string) error {
+	usaToday := strings.ToLower(time.Now().Format("Monday-1-2-2006"))
+	euToday := strings.ToLower(time.Now().Format("2006-01-02")) // 2006 January 2nd
+	urlToday := fmt.Sprintf("%s%s/", urlPrefix, usaToday)
+	println("Downloading ", urlToday)
+
+	req, err := http.NewRequest("GET", urlToday, nil)
+	if err != nil {
+		return err
+	}
+
+	req = req.WithContext(ctx)
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+	f, err := os.Create(fmt.Sprintf("/home/pi/comptrain/%s_%s.html", filePrefix, euToday))
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	if n, err := io.Copy(f, resp.Body); err != nil {
+		return err
+	} else {
+		fmt.Printf("Downloaded HTML for %s with size %2.fkb\n", usaToday, float64(n)/1024)
+	}
+	return nil
+}
